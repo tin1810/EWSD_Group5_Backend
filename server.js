d2128a178a9a4f50e09561bcf1dd958decd98a4e
Zack <109455683+zawminhtwe001@users.noreply.github.com>
2025-03-22 15:24:24 +0630

    Update all files

--- server.js
+++ server.js
@@ -9,28 +9,7 @@ const archiver = require('archiver');
 require('dotenv').config();
 
 
-const dbUrl = process.env.DB_URL;
-const parsedUrl = new URL(dbUrl);
 
-const connection = mysql.createConnection({
-  host: parsedUrl.hostname,
-  user: parsedUrl.username,
-  password: parsedUrl.password,
-  database: parsedUrl.pathname.split('/')[1],
-  port: parsedUrl.port
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("Database connection error:", err);
-    return;
-  }
-  console.log("✅ Connected to MySQL database!");
-});
-
-module.exports = connection;
-
-// end
 
 const app = express();
 app.use(express.json());
